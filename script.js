d911fc378efc29152320ccf4e1d93bb909ca4786
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-05-07 12:52:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let t=document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead)
+console.log(t.tHead.firstElementChild)
+console.log(t.tBodies)
+console.log(t.rows[1].rowIndex)
+
+let t=document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead)
+console.log(t.tHead.firstElementChild)
+console.log(t.tBodies)
+console.log(t.rows[1].rowIndex)
+
+let t=document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead)
+console.log(t.tHead.firstElementChild)
+console.log(t.tBodies)
+console.log(t.rows[1].rowIndex)
